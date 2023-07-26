59e9917f375e84e983830a61155531924234a77e
Miguel Urena <urena_miguel82@outlook.com>
2023-07-26 12:05:32 -0700

    Creating article route

---
+++ routes/articles.js
@@ -0,0 +1,5 @@
+// All routes that are related to articles
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 // Coding views in EJS
 app.set('view engine', 'ejs');
+
+// Use the article router
+app.use('/articles', articleRouter);
 
 // This render from index.ejs to website
 app.get('/', (req, res) => {
