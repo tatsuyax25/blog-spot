67fd5af0ae7bf6cb6819cbb4a5a6e7f2a8e24a25
Miguel Urena <urena_miguel82@outlook.com>
2023-07-28 10:15:21 -0700

    Create article card

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
     title: 'Test Article',
     createdAt: Date.now(),
     description: 'Test description'
+  },
+  {
+    title: 'Test Article 2',
+    createdAt: Date.now(),
+    description: 'Test description 2'
   }];
   res.render('index', { articles: articles });
 });
